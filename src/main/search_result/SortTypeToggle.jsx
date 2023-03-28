9572cbeea4401a61e23ae072272936851ee36e40
boxak <boxak@naver.com>
2023-03-28 21:59:25 +0900

    2023-03-28

--- src/main/search_result/SortTypeToggle.jsx
+++ src/main/search_result/SortTypeToggle.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const SortTypeToggle = props => {
-
+//
   useEffect(() => {
     props.setToggleType(props.toggleType);
   }, [props.toggleType]);
